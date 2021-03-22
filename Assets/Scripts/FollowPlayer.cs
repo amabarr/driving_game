ba4dd9279cef42ae7420a5cb2b0d28ce10ff1125
Amanda Barrafato <amb1047@nyu.edu>
2021-03-22 16:05:25 -0400

    Lesson 1.3 completed

--- Assets/Scripts/FollowPlayer.cs
+++ Assets/Scripts/FollowPlayer.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class FollowPlayer : MonoBehaviour
 {
   public GameObject player;
+  private Vector3 offset = new Vector3(0, 5, -7);
   // Start is called before the first frame update
   void Start()
   {
@@ -15,6 +16,6 @@ public class FollowPlayer : MonoBehaviour
   void Update()
   {
     //offset the camera behind and above the player
-    transform.position = player.transform.position + new Vector3(0, 5, -7);
+    transform.position = player.transform.position + offset;
   }
 }
